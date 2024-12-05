72fa6c5611bcb51815b674f6116e3be27f543755
Sem4kok <semkremnev@team.amocrm.com>
2024-12-05 20:53:56 +0300

    feat(p2p): Rpc added

--- internal/p2p/decoder/default_decoder/default_decoder.go
+++ internal/p2p/decoder/default_decoder/default_decoder.go
@@ -1,19 +1,19 @@
 package decoder
 
 import (
-	"github.com/Sem4kok/DFS/internal/p2p/decoder"
+	"github.com/Sem4kok/DFS/internal/p2p/message"
 	"io"
 )
 
 type DefaultDecoder struct{}
 
-func (d *DefaultDecoder) Decode(r io.Reader, msg *decoder.Message) error {
+func (d *DefaultDecoder) Decode(r io.Reader, rpc *message.RPC) error {
 	buf := make([]byte, 1028)
 	n, err := r.Read(buf)
 	if err != nil {
 		return err
 	}
 
-	msg.Payload = buf[:n]
+	rpc.Payload = buf[:n]
 	return nil
 }
