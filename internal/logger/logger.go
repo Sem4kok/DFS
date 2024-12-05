098d41a6cc3ef10085441d8784a38e476a37a89f
Sem4kok <semkremnev@team.amocrm.com>
2024-12-05 19:27:11 +0300

    feat(logger): NOPLogger added`

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -8,3 +8,14 @@ type Logger interface {
 	Fatal(args ...interface{})
 	With(args ...interface{}) Logger
 }
+
+type NOPLogger struct{}
+
+func (l *NOPLogger) Debug(args ...interface{}) {}
+func (l *NOPLogger) Info(args ...interface{})  {}
+func (l *NOPLogger) Warn(args ...interface{})  {}
+func (l *NOPLogger) Error(args ...interface{}) {}
+func (l *NOPLogger) Fatal(args ...interface{}) {}
+func (l *NOPLogger) With(args ...interface{}) Logger {
+	return nil
+}
