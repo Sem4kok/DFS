db5028c4c0a9aa2697e7f3bb9c6b9b3e3306c0c2
Sem4kok <semkremnev@team.amocrm.com>
2024-12-05 20:53:38 +0300

    feat(p2p): Consume method added

--- internal/p2p/transport/transport.go
+++ internal/p2p/transport/transport.go
@@ -1,7 +1,12 @@
 package transport
 
+import (
+	"github.com/Sem4kok/DFS/internal/p2p/message"
+)
+
 // Peer is an interface that represents remote node
 type Peer interface {
+	Close() error
 }
 
 // Transport is handler that connects two or more
@@ -9,5 +14,6 @@ type Peer interface {
 // (TCP, UDP, websocket, gRPC, etc...)
 type Transport interface {
 	ListenAndAccept() error
+	Consume() <-chan message.RPC
 	Shutdown()
 }
